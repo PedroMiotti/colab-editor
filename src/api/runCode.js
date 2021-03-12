572b9e1eb103c3ac93fdafa01852e74fbc14e6d4
Pedro Miotti <pedromiotti7@gmail.com>
2021-03-11 22:41:38 -0300

    First commit

---
+++ src/api/runCode.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+
+
+export const runCode = async (source_code,stdin, language_id) => {
+    let token;
+    let solutionJson;
+
+    const response = await axios
+      .post(`http://localhost/submissions`, {
+        source_code,
+        stdin,
+        language_id,
+      })
+      .then((res) => {
+        token = res.data.token;
+      });
+
+    let jsonGetSolution = {
+      status: { description: "Queue" },
+      stderr: null,
+      compile_output: null,
+    };
+
+    while (
+      jsonGetSolution.status.description !== "Accepted" &&
+      jsonGetSolution.stderr == null &&
+      jsonGetSolution.compile_output == null
+    ) {
+      if (token) {
+        const getResult = await axios
+          .get(`http://localhost/submissions/${token}`)
+          .then((res) => {
+            jsonGetSolution = res.data;
+            return jsonGetSolution;
+
+          });
+      }
+    }
+    return jsonGetSolution;
+  };
