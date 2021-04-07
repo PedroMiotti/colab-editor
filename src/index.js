6a9ae0c9d14df2f9c107d206238cc99143c3a062
Pedro Miotti <pedromiotti7@gmail.com>
2021-04-06 23:19:20 -0300

    Create room implemented

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from "react-dom";
 
 import Routes from './Router';
 
+import 'antd/dist/antd.css';
 import "./style.css";
 
 // Contexts
 import RoomState from "./context/room/room.state.jsx";
-// import { socket, SocketContext } from "./context/socket.js";
 import { SocketContextProvider } from "./context/socket.js";
 import { NamespaceContextProvider } from "./context/namespace.js";
 
--- src/pages/salas/index.jsx
+++ src/pages/salas/index.jsx
@@ -6,25 +6,47 @@ import CreateRoomModal from "../../components/CreateRoomModal";
 import { useRoomContext } from "../../context/room/room.context";
 import { NamespaceContext } from "../../context/namespace.js";
 
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
+
+import { message } from "antd";
 
 const PaginaInicial = () => {
-  const { createRoom } = useRoomContext();
+  const { createRoom, joinRoom } = useRoomContext();
   const [nspName, setNamespace] = useContext(NamespaceContext);
 
-  const [ sessionCode, setSessionCode ] = useState('');
+  const [sessionCode, setSessionCode] = useState("");
 
-  const createNamespace = () => {
-    const genNspId = uuidv4();
-    setNamespace(genNspId);
-    sessionStorage.setItem('Namespace', genNspId);
+  const setNamespaceId = (namespaceId) => {
+    return new Promise((resolve) => {
+      sessionStorage.setItem("Namespace", namespaceId);
+      setNamespace(namespaceId)
+      resolve();
+    })
   };
 
-  const joinNamespace = () => {
+  const createNamespace = async () => {
+    const genNspId = uuidv4();
+
+
+    await setNamespaceId(genNspId);
+    createRoom(genNspId, "Pedro")
+    // sessionStorage.setItem("Namespace", genNspId);
+    // setNamespace(genNspId);
+  };
+
+
+  const joinNamespace = async () => {
     const codeToJoin = sessionCode;
-    setNamespace(codeToJoin);
-    sessionStorage.setItem('Namespace', codeToJoin);
-  }
+
+    joinRoom(codeToJoin, "Thiago")
+      .then(() => {
+        setNamespace(codeToJoin);
+        sessionStorage.setItem("Namespace", codeToJoin);
+      })
+      .catch((e) => {
+        message.error(e);
+      });
+  };
 
   const createSession = () => {
     createRoom({ username: "Pedro", roomName: "Tech" });
@@ -32,32 +54,34 @@ const PaginaInicial = () => {
 
   const handleInput = (e) => {
     setSessionCode(e.target.value);
-  }
-  
+  };
+
   return (
-    <div className="paginaSalasContainer">
-      <div className="boasVindas">
-        <h1>
-          Olá, <strong>FShinoda</strong>!
-        </h1>
-      </div>
-
-      <div className="caixas">
-        <div className="criarSala">
-          <h2>Criar Sala</h2>
-
-          <a onClick={createNamespace}>&#43;</a>
+    <>
+      <div className="paginaSalasContainer">
+        <div className="boasVindas">
+          <h1>
+            Olá, <strong>FShinoda</strong>!
+          </h1>
         </div>
 
-        <div className="entrarSala">
-          <h2>Entrar em uma sala</h2>
+        <div className="caixas">
+          <div className="criarSala">
+            <h2>Criar Sala</h2>
 
-          <input onChange={handleInput} type="text" spellCheck="false" />
+            <a onClick={createNamespace}>&#43;</a>
+          </div>
 
-          <a onClick={joinNamespace}>Entrar</a>
+          <div className="entrarSala">
+            <h2>Entrar em uma sala</h2>
+
+            <input onChange={handleInput} type="text" spellCheck="false" />
+
+            <a onClick={joinNamespace}>Entrar</a>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
