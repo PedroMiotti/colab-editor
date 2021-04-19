13552298da4e7d32b314ec24de32889edd75bbaf
João Kitajima <joao.kitajima@outlook.com>
2021-04-19 12:11:48 -0300

    alter front namePage

--- src/pages/nomes/index.jsx
+++ src/pages/nomes/index.jsx
@@ -1,19 +1,30 @@
-import React from 'react';
-import './styles.css';
+import React from "react";
+import "./styles.css";
+
+// Ant Design
+import { Button } from 'antd';
 
 const PaginaNome = () => {
     return(
         <div id="paginaNome">
-            <div id="logo">
-                <h1>COLAB EDITOR</h1>
-            </div>
+            <div className="noise_namePage"></div>
 
-            <div id="caixaNome">
-                <h1>Insira seu nome</h1>
+            <div className="overlay_namePage"></div>
 
-                <input type="text" spellcheck="false" maxLength="20" aria-required></input>
+            <div className="terminal_namePage">
+                <div id="logo">
+                    <h1 className="output_errorPage"><span className="errorcode_errorPage">COLAB EDITOR</span></h1>
+                </div>
 
-                <a>Confirmar</a>
+                <div id="caixaNome">
+                    <h1>Insira seu nome</h1>
+
+                    <input type="text" spellcheck="false" maxLength="20" aria-required></input>
+
+                    <div id="cnfBtn">
+                        <Button type="primary">Confirmar</Button>
+                    </div>
+                </div>
             </div>
         </div>
     );
