cd9f82e7f542a32e09fb40f07fdf4d5431f29fc7
Pedro <pedromiotti7@gmail.com>
2021-06-16 18:48:16 -0300

    Removed name page

--- src/pages/salas/index.jsx
+++ src/pages/salas/index.jsx
@@ -1,147 +1,176 @@
-import React, { useState, useContext, useEffect } from "react";
+import React, {useState, useContext, useEffect, useRef} from "react";
 import "./style.css";
 import "../../assets/style/backgroundEffect.css";
 
 // Ant Design
-import { Button, notification, message } from 'antd';
-import { LoadingOutlined } from '@ant-design/icons';
+import {Button, notification} from 'antd';
+import {LoadingOutlined} from '@ant-design/icons';
 
 // Context
-import { useRoomContext } from "../../context/room/room.context";
-import { NamespaceContext } from "../../context/namespace.js";
+import {useRoomContext} from "../../context/room/room.context";
+import {NamespaceContext} from "../../context/namespace.js";
 
-import { v4 as uuidv4 } from "uuid";
+import {v4 as uuidv4} from "uuid";
+
+// Components
+import {customNotification} from '../../components/Notification';
 
 const PaginaInicial = () => {
-  const { createRoom, joinRoom, checkForExistingRoomAndUsername } = useRoomContext();
+    const {createRoom, joinRoom, checkForExistingRoomAndUsername} = useRoomContext();
 
-  const [ nspName, setNamespace ] = useContext(NamespaceContext);
-  const [ confirmCreateRoom, setConfirmCreateRoom ] = useState(false);
-  const [ confirmJoinRoom, setConfirmJoinRoom ] = useState(false);
-  const [ sessionCode, setSessionCode ] = useState("");
+    const [nspName, setNamespace] = useContext(NamespaceContext);
+    const [confirmCreateRoom, setConfirmCreateRoom] = useState(false);
+    const [confirmJoinRoom, setConfirmJoinRoom] = useState(false);
+    const [sessionCode, setSessionCode] = useState("");
+    const [nameInput, setNameInput] = useState("");
 
-  const username = localStorage.getItem('username');
+    const username = localStorage.getItem('username');
 
-  useEffect(() => {
-    if(nspName)
-      createRoom(nspName, username);
+    const nameInputRef = useRef();
 
-  }, [confirmCreateRoom])
+    useEffect(() => {
+        if (nspName)
+            createRoom(nspName, username);
 
-  useEffect(() => {
-    if(nspName && confirmJoinRoom)
-      joinRoom(nspName, username);
+    }, [confirmCreateRoom])
 
-  }, [confirmJoinRoom])
+    useEffect(() => {
+        if (nspName && confirmJoinRoom)
+            joinRoom(nspName, username);
 
-  const setNamespaceId = (namespaceId) => {
-    sessionStorage.setItem("Namespace", namespaceId);
-    setNamespace(namespaceId);
-  };
+    }, [confirmJoinRoom])
 
+    useEffect(() => {
+        nameInputRef.current.focus();
+    }, [])
 
-  const createNamespace = () => {
-    const genNspId = uuidv4();
-    setNamespaceId(genNspId)
+    const setNamespaceId = (namespaceId) => {
+        sessionStorage.setItem("Namespace", namespaceId);
+        setNamespace(namespaceId);
+    };
 
-    notification.open({
-      message : "Sala criada com sucesso!",
-      description : "Você será redirecionado assim que esta janela for fechada!",
-      onClose: (() => setConfirmCreateRoom(true)),
-      icon : <LoadingOutlined style={{ color: "#161616" }} />,
-      className : "createRoomNotification",      
-      placement : "bottomLeft",
-      bottom : 50,
-      duration : 1.5,
-      rtl : true
-    });
-  };
+    const handleNameInput = (e) => {
+        setNameInput(e.target.value);
+    };
 
-  // Check if the namespace is valid && if the username is already in use
-  const joinNamespace = () => {
-    const codeToJoin = sessionCode;
+    const createNamespace = () => {
+        if (nameInput === "") {
+            customNotification("error", "topRight", `Ops, Insira seu nome primeiro`, "copyNotification", 2.5);
+        } else {
+            localStorage.setItem("username", nameInput);
+            setTimeout(() => {
+                const genNspId = uuidv4();
+                setNamespaceId(genNspId)
 
-    checkForExistingRoomAndUsername(codeToJoin, username)
-      .then(() => {
-        setNamespaceId(codeToJoin);
+                notification.open({
+                    message: "Sala criada com sucesso!",
+                    description: "Você será redirecionado assim que esta janela for fechada!",
+                    onClose: (() => setConfirmCreateRoom(true)),
+                    icon: <LoadingOutlined style={{color: "#161616"}}/>,
+                    className: "createRoomNotification",
+                    placement: "bottomLeft",
+                    bottom: 50,
+                    duration: 1.5,
+                    rtl: true
+                });
+            }, 1000)
+        }
+    };
 
-        notification.open({
-          message : "Entrando na sala !",
-          description : "Você será redirecionado assim que esta janela for fechada!",
-          onClose: (() => setConfirmJoinRoom(true)),
-          icon : <LoadingOutlined style={{ color: "#111c30" }} />,
-          className : "createRoomNotification",      
-          placement : "bottomRight",
-          bottom : 50,
-          duration : 1.5,
-          rtl : true
-        });
+    // Check if the namespace is valid && if the username is already in use
+    const joinNamespace = () => {
+        const codeToJoin = sessionCode;
 
-      })
-      .catch((e) => {
-        message.error(e);
-      });
-  };
+        if (nameInput === "") {
+            customNotification("error", "topRight", `Ops, Insira seu nome primeiro`, "copyNotification", 2.5);
+        } else {
+            localStorage.setItem("username", nameInput);
 
-  const handleInput = (e) => {
-    setSessionCode(e.target.value);
-  };
+            setTimeout(() => {
+                checkForExistingRoomAndUsername(codeToJoin, username)
+                    .then(() => {
+                        setNamespaceId(codeToJoin);
 
-  return (
-    <>
-      <div id="roomPage">
-        <div className="backgroundEffect">
-          <div className="noise"></div>
+                        notification.open({
+                            message: "Entrando na sala !",
+                            description: "Você será redirecionado assim que esta janela for fechada!",
+                            onClose: (() => setConfirmJoinRoom(true)),
+                            icon: <LoadingOutlined style={{color: "#111c30"}}/>,
+                            className: "createRoomNotification",
+                            placement: "bottomRight",
+                            bottom: 50,
+                            duration: 1.5,
+                            rtl: true
+                        });
+                    })
+                    .catch((e) => {
+                        customNotification("error", "topRight", `${e} ☹️ `, "copyNotification", 2.5);
+                    });
+            }, 1000)
 
-          <div className="overlay"></div>
+        }
+    };
 
-          <div className="terminal terminal_roomPage">
-            <div className="boasVindas">
-              <h1>
-                Olá, <span>{username}</span>!
-              </h1>
+    const handleInput = (e) => {
+        setSessionCode(e.target.value);
+    };
+
+    return (
+        <>
+            <div id="roomPage">
+                <div className="backgroundEffect">
+                    <div className="noise"></div>
+
+                    <div className="overlay"></div>
+
+                    <div className="terminal terminal_roomPage">
+                        <div className="boasVindas">
+                            <h1>
+                                Olá,<input ref={nameInputRef} onChange={handleNameInput} className="roomPage-nameInput"
+                                           placeholder="Seu nome aqui" maxlength="10"/>
+                            </h1>
+                        </div>
+
+                        <div className="caixas">
+                            <div className="card">
+                                <div className="face face1">
+                                    <div className="contentHeader">
+                                        <h2>Criar Sala</h2>
+                                    </div>
+                                </div>
+
+                                <div className="face face2">
+                                    <div className="content">
+                                        <Button id="but-criar" type="link" onClick={createNamespace}>
+                                            &#43;
+                                        </Button>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="card">
+                                <div className="face face1">
+                                    <div className="contentHeader">
+                                        <h2>Encontrar Sala</h2>
+                                    </div>
+                                </div>
+
+                                <div className="face face2">
+                                    <div className="content">
+                                        <input onChange={handleInput} type="text" spellCheck="false"/>
+
+                                        <a id="but-entrar" onClick={joinNamespace}>
+                                            Entrar
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-
-            <div className="caixas">
-              <div className="card">
-                <div className="face face1">
-                  <div className="contentHeader">
-                    <h2>Criar Sala</h2>
-                  </div>
-                </div>
-
-                <div className="face face2">
-                  <div className="content">
-                    <Button id="but-criar" type="link" onClick={createNamespace}>
-                      &#43;
-                    </Button>
-                  </div>
-                </div>
-              </div>
-
-              <div className="card">
-                <div className="face face1">
-                  <div className="contentHeader">
-                    <h2>Encontrar Sala</h2>
-                  </div>
-                </div>
-
-                <div className="face face2">
-                  <div className="content">
-                    <input onChange={handleInput} type="text" spellCheck="false" />
-
-                    <a id="but-entrar" onClick={joinNamespace}>
-                      Entrar
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+        </>
+    );
 };
 
 export default PaginaInicial;
--- src/router.js
+++ src/router.js
@@ -8,19 +8,15 @@ import Editor from "./pages/editor/index";
 //Pagina escolha sala
 import MenuSala from "./pages/salas/index";
 
-// Página para inserir nome de usuário
-import PaginaNome from './pages/nomes/index.jsx';
-
 // Página de Erro
 import PaginaErro from './pages/error_404/index.jsx';
 
 const Router = () => {
   return (
     <Switch>
-      <Route exact path="/menu" component={MenuSala} />
+      <Route exact path={["/menu", "/"]} component={MenuSala} />
       <Route exact path="/editor" component={Editor} />
       <Route exact path="/editor/:namespaceId" component={Editor} />
-      <Route exact path={["/nome", "/"]} component={PaginaNome} />
       <Route exact path="/erro" component={PaginaErro} />
       <Redirect to="/erro"/>
     </Switch>
