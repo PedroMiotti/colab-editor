9db12331517a66e6130b723429dbcd0c0f767e60
Pedro <pedromiotti7@gmail.com>
2021-06-17 13:56:49 -0300

    Logo change

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,64 +1,70 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import './style.css';
 
 // Router-dom
-import { useParams } from "react-router-dom";
+import {useParams} from "react-router-dom";
 
 // Assets
-// import { runCode } from "../../api/runCode.js";
+import Logo40x40 from '../../assets/imgs/logo40x40.png'
+import LogoPurple from '../../assets/imgs/logo6F40A7.svg';
+import LogoPink from '../../assets/imgs/logoF1D4D4.svg';
+import LogoYellow from '../../assets/imgs/logoFFBC50.svg';
+import LogoWhite from '../../assets/imgs/logoWhite60.svg';
+
 
 // Components
-import { customNotification } from '../Notification';
+import {customNotification} from '../Notification';
 
 // Icons
 import {
     CaretDownOutlined,
     CaretRightOutlined,
     CopyFilled,
-  } from "@ant-design/icons";
+} from "@ant-design/icons";
 
 
-const Navbar = ({ runCode }) => {
-  const [language, setLanguage] = useState({ id: "63", name: "javascript" });
-  const [theme, setTheme] = useState("vs-dark");
-  const [codeToSubmit, setCodeToSubmit] = useState("");
-  const [stdin, setStdin] = useState("");
+const Navbar = ({runCode}) => {
+    const [language, setLanguage] = useState({id: "63", name: "javascript"});
+    const [theme, setTheme] = useState("vs-dark");
+    const [codeToSubmit, setCodeToSubmit] = useState("");
+    const [stdin, setStdin] = useState("");
 
-  const username = localStorage.getItem("username");
+    const username = localStorage.getItem("username");
 
-  // copy id to clipboard
-  const { namespaceId } = useParams();
-  const url = namespaceId;
+    // copy id to clipboard
+    const {namespaceId} = useParams();
+    const url = namespaceId;
 
-  const copyToClipboard = (e) => {
-    navigator.clipboard.writeText(e.target.value);
-    customNotification("success", "bottomRight", "Copiado para a área de transferência", "copyNotification", 1.5  );
-  };
+    const copyToClipboard = (e) => {
+        navigator.clipboard.writeText(e.target.value);
+        customNotification("success", "bottomRight", "Copiado para a área de transferência", "copyNotification", 1.5);
+    };
 
-  return (
-    <div id="navbar-editor">
-      <div className="navbar-main">
-        <h1>ALL.CO</h1>
-      </div>
+    return (
+        <div id="navbar-editor">
+            <div className="navbar-main">
+               <img src={LogoPurple}/>
+                <h1>{'COLAB_'}</h1>
+            </div>
 
-      <div className="navbar-config">
+            <div className="navbar-config">
 
-        <div className="run"></div>
+                <div className="run"></div>
 
-        <button className="run-button" onClick={runCode}>
-          <CaretRightOutlined className="play-ico" />
-        </button>
-      </div>
+                <button className="run-button" onClick={runCode}>
+                    <CaretRightOutlined className="play-ico"/>
+                </button>
+            </div>
 
-      <div className="navbar-info">
-        <h2>{username}/</h2>
-        <button className="btn-copy" value={url} onClick={copyToClipboard}>
-          <CopyFilled className="copy-icon" />
-          Link
-        </button>
-      </div>
-    </div>
-  );
+            <div className="navbar-info">
+                <h2>{username}/</h2>
+                <button className="btn-copy" value={url} onClick={copyToClipboard}>
+                    <CopyFilled className="copy-icon"/>
+                    Link
+                </button>
+            </div>
+        </div>
+    );
 };
 
 export default Navbar;
--- src/pages/editor/components/Welcome/index.jsx
+++ src/pages/editor/components/Welcome/index.jsx
@@ -3,12 +3,16 @@ import './style.css';
 
 import CreateFileInstructions from '../../../../assets/imgs/createFileIntruction.png';
 import CopyLinkInstruction from '../../../../assets/imgs/copyLinkInstruction.png';
+import LogoWhite from '../../../../assets/imgs/logoWhite60.svg';
+import LogoPurple from '../../../../assets/imgs/logo6F40A7.svg';
 
 
 const Welcome = () => {
     return(
         <div className="welcome-page-container">
+            {/*<img style={{width: "100px", marginRight: "20px", marginBottom: "20px", alignSelf: "center"}} src={LogoPurple} />*/}
             <h1>Bem-vindo !</h1>
+
 
             <p>01. Para começar crie um novo arquivo</p>
             <div className="welcome-page-img-container">
